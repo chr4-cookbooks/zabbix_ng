c1dcb337ceb66740d4e47c792b43ed61f9a9a239
Chris Aumann <me@chr4.org>
2016-06-30 15:16:19 +0200

    Add source_url and issues_url to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'me@chr4.org'
 license          'GNU Public License 3.0'
 description      'Installs/Configures zabbix_ng'
 long_description 'Installs/Configures zabbix_ng'
+source_url       'https://github.com/chr4-cookbooks/zabbix_ng' if respond_to?(:source_url)
+issues_url       'https://github.com/chr4-cookbooks/zabbix_ng/issues' if respond_to?(:issues_url)
 version          '1.0.3'
 depends          'apt'
 depends          'sudo'
